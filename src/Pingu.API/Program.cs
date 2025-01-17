63eb0ed46a68c3ea0aaffbe0b55b8547cdb1717f
ryan <ryan.bromati@hotmail.com>
2025-01-17 00:05:47 -0300

    feat: adicionar referências de projeto e configurar serviços de infraestrutura no Pingu.API

--- src/Pingu.API/Program.cs
+++ src/Pingu.API/Program.cs
@@ -1,9 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Pingu.Infrastructure;
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
+app.UseExceptionHandler("/error");
 app.Run();
