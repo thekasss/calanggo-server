89f7dc805848ca6ad26864ef1cc4c8167a08c22b
ryan <ryan.bromati@hotmail.com>
2025-01-20 12:13:54 -0300

    feat: refactor URL shortening endpoint to use handler method and add response descriptions

--- src/Pingu.API/Endpoints/UrlShortenerEndpoint.cs
+++ src/Pingu.API/Endpoints/UrlShortenerEndpoint.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 using Pingu.Application.Interfaces;
 using Pingu.Application.UseCases.ShortenUrl;
@@ -11,22 +12,33 @@ public static class UrlShortenerEndpoint
     {
         var urlShortenerEndpoint = endpoinBuilder.MapGroup("/url-shortener");
 
-        urlShortenerEndpoint.MapPost("/shorten", async (HttpContext context, IUrlShortenerService urlShortenerService, ShortenUrlRequest request) =>
-        {
-            var result = await urlShortenerService.CreateShortenedUrl(request.OriginalUrl, request.CreatedBy, request.ExpiresAt);
-            if (result.IsSuccess == false)
-            {
-                return Results.Problem(
-                    title: "An error occurred while shortening the URL",
-                    statusCode: result.Error!.Code,
-                    detail: result.Error!.Message);
-            }
+        // POST /url-shortener/shorten
+        urlShortenerEndpoint.MapPost("/shorten", HandleShortenUrl)
+            .WithDescription("The endpoint to shorten a URL")
+            .Produces<ProblemDetails>(400)
+            .Produces<ShortenUrlResponse>(201);
 
-            string shortenedUrl = context.Request.GetDisplayUrl().Replace(context.Request.Path, $"/{result.Value!.ShortCode}");
-            var response = new ShortenUrlResponse(result.Value.OriginalUrl, shortenedUrl, result.Value.ShortCode, result.Value.ExpiresAt);
-            return Results.Created(shortenedUrl, response);
-        });
 
         return urlShortenerEndpoint;
     }
+    #region [Endpoints Handlers]
+
+    // POST /url-shortener/shorten
+    private static async Task<IResult> HandleShortenUrl(HttpContext context, IUrlShortenerService urlShortenerService, ShortenUrlRequest request)
+    {
+        var result = await urlShortenerService.CreateShortenedUrl(request.OriginalUrl, request.CreatedBy, request.ExpiresAt);
+        if (result.IsSuccess == false)
+        {
+            return Results.Problem(
+                title: "An error occurred while shortening the URL",
+                statusCode: result.Error!.Code,
+                detail: result.Error!.Message);
+        }
+
+        string shortenedUrl = context.Request.GetDisplayUrl().Replace(context.Request.Path, $"/{result.Value!.ShortCode}");
+        var response = new ShortenUrlResponse(result.Value.OriginalUrl, shortenedUrl, result.Value.ShortCode, result.Value.ExpiresAt);
+        return Results.Created(shortenedUrl, response);
+    }
+
+    #endregion
 }
